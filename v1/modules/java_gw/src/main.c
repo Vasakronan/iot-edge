ebf3f4d67f393a5e9c07c7cc732520ff72ce1767
Andriy Yatsyshyn <abyatsyshyn@gmail.com>
2019-06-05 16:45:03 +0300

    Update main.c

--- v1/modules/java_gw/src/main.c
+++ v1/modules/java_gw/src/main.c
@@ -8,33 +8,35 @@
 
 int main(int argc, char** argv)
 {
-    GATEWAY_HANDLE gateway;
-    if (argc != 2)
-    {
-        printf("usage: java_gw configFile\n");
-        printf("where configFile is the name of the file that contains the Gateway configuration\n");
-    }
-    else
-    {
-        if (platform_init() == 0)
-        {
-            if ((gateway = Gateway_CreateFromJson(argv[1])) == NULL)
-            {
-                printf("failed to create the gateway from JSON\n");
-            }
-            else
-            {
-                printf("gateway successfully created from JSON\n");
-                printf("gateway shall run until ENTER is pressed\n");
-                (void)getchar();
-                Gateway_Destroy(gateway);
-            }
-            platform_deinit();
-        }
-        else
-        {
-            printf("Failed to initialize the platform.");
-        }
-    }
-    return 0;
+   GATEWAY_HANDLE gateway;
+   if (argc != 2)
+   {
+       printf("usage: java_gw configFile\n");
+       printf("where configFile is the name of the file that contains the Gateway configuration\n");
+   }
+   else
+   {
+       if (platform_init() == 0)
+       {
+           if ((gateway = Gateway_CreateFromJson(argv[1])) == NULL)
+           {
+               printf("failed to create the gateway from JSON\n");
+           }
+           else
+           {
+               printf("gateway successfully created from JSON\n");
+               printf("gateway shall run until ENTER is pressed\n");
+               do {
+                   c = getchar();
+               } while (c != 5);
+               Gateway_Destroy(gateway);
+           }
+           platform_deinit();
+       }
+       else
+       {
+           printf("Failed to initialize the platform.");
+       }
+   }
+   return 0;
 }
